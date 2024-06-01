dff228b9f6c4be60554535d9e71b6090fb617aec
gauri <gaurigera@gmail.com>
2024-06-02 02:17:05 +0530

    added socket.io implementation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
 const { Server } = require("socket.io");
 
 const app = express();
 const port = 3000;
+const io = new Server(app);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
