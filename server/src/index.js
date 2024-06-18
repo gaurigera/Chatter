e8b8c18305b0cde556f92f83572eee0533b05569
gauri <gaurigera@gmail.com>
2024-06-18 11:33:09 +0530

    added apollo server

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,40 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import cors from "cors";
+import { ApolloServer } from "@apollo/server";
 
 const app = express();
-const port = 3000;
+const port = 4000;
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+const server = new ApolloServer({
+    typeDefs: `
+        type A {
+            id: ID!
+        }
+        type Query {
+            getA: [A]
+        }
+    `,
+    resolvers : {},
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
+// Ensure we wait for our server to start
+await server.start();
 
-httpServer.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+app.use("/", cors(), express.json(), expressMiddleware(server));
+
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: port }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:${port}/`);
 
 httpServer.on("listening", () => {
     import("./socket/connection.js");
-})
+});
 
 export { io };
