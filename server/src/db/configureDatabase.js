573b8932a50fabd8061fadf50e1489d9bc441963
gauri <gaurigera@gmail.com>
2024-08-26 23:55:15 +0530

    set up: drizzle orm

--- server/src/db/configureDatabase.js
+++ server/src/db/configureDatabase.js
@@ -1,9 +1,13 @@
 import { sql } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
+import dotenv from 'dotenv';
+
+// to load environment variables from .env file
+dotenv.config();
+
 try {
-    
-    const queryClient = postgres("postgres://postgres:postgres@0.0.0.0:5432/chatter");
+    const queryClient = postgres(process.env.POSTGRES_CONNECTION_STRING);
     const db = drizzle(queryClient)
     
     const res = await db.execute(sql`select * from users`)
